2e83ffc7d1d7bfae894870008938121f31eaed4e
Kazuo Tsubaki <kazz12211@gmail.com>
2019-08-30 02:01:45 +0900

    Part 1

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quam voluptatum repellendus ratione accusamus. Voluptatem sapiente laudantium exercitationem. Rerum, esse, mollitia neque minus quia natus cumque, quae deleniti architecto laborum eaque atque.',
+            'author': 'Author 1',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quam voluptatum repellendus ratione accusamus. Voluptatem sapiente laudantium exercitationem. Rerum, esse, mollitia neque minus quia natus cumque, quae deleniti architecto laborum eaque atque.',
+            'author': 'Author 2',
+            'create_date': '09-11-2018'
+       },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quam voluptatum repellendus ratione accusamus. Voluptatem sapiente laudantium exercitationem. Rerum, esse, mollitia neque minus quia natus cumque, quae deleniti architecto laborum eaque atque.',
+            'author': 'Author 3',
+            'create_date': '03-11-2011'
+        }
+    ]
+    return articles
