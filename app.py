f76302278947e349d473e8389d8de85d3de22739
Kazuo Tsubaki <kazz12211@gmail.com>
2019-08-30 21:58:24 +0900

    added login page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Articles
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 import datetime
 
 app = Flask(__name__)
@@ -41,26 +42,6 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
-        email = form.email.data
-        if db.session.query(User).filter(User.name == name or User.username == username).count() == 0:
-            data = User(name, username, password, email)
-            db.session.add(data)
-            db.session.commit()
-            flash('You are now registered and can login', 'success')
-            return redirect(url_for('login'))
-        else:
-            flash('You are already registered', 'warning')
-            return redirect(url_for('register'))
-
-    return render_template('register.html', form=form)
-
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -76,6 +57,73 @@ def articles():
 @app.route('/article/<string:id>')
 def article(id):
     return render_template('article.html', id = id)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        email = form.email.data
+        if db.session.query(User).filter(User.name == name or User.username == username).count() == 0:
+            data = User(name, username, password, email)
+            db.session.add(data)
+            db.session.commit()
+            flash('You are now registered and can login', 'success')
+        else:
+            flash('You are already registered', 'warning')
+            return redirect(url_for('register'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        user = db.session.query(User).filter(User.username == username).first()
+        if user is None:
+            error = 'Invalid login'
+            app.logger.info('NO USER')
+            return render_template('login.html', error = error)
+        else:
+            password = user.password
+            if sha256_crypt.verify(password_candidate, password):
+                app.logger.info('PASSWORD MATCHED')
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                app.logger.info('PASSWORD NOT MATCHED')
+                error = 'Invalid login'
+                return render_template('login.html', error = error)
+    return render_template('login.html')
+
+## Check user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.debug = True
